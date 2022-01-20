a15d977b3b816e451b480c3607a6e23e4d40edd9
이상현 <skaksdmltj@naver.com>
2022-01-21 04:37:01 +0900

    2진수를 10진수로 변경.

--- practice.c
+++ practice.c
@@ -5,10 +5,18 @@
 
 int main(){
 	
-	printf("ddd");
+	int array[4] = { 1,1,0,1 };
+	int count = 0;
+	double pow_sum = 0;
 
+	for (int i = sizeof(array) / sizeof(int) - 1; i >= 0; i--) {
+		if (array[i]) {
+			pow_sum += pow(2, count);
+		}
+		count++;
+	}
+	printf("%.0f", pow_sum);
 
-	//새로 병합해보자//
 
 	return 0;
 }
