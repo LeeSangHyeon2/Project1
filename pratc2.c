d5f00f49bb9b575c6a3e3f9a651d762b52033fd2
이상현 <skaksdmltj@naver.com>
2022-01-21 04:45:49 +0900

    두번째 추가

---
+++ pratc2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+
+
+int main() {
+
+	//새로운추가//
+
+
+	return 0;
+}
---
+++ pratice2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+
+
+int main() {
+
+	printf("연습공간");
+
+	return 0;
+}
